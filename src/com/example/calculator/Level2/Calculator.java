97e10f9c84ddf4128d38e20b5ff8354f3e0f8a3d
sumyeom <wjstndus75@naver.com>
2024-10-16 18:29:29 +0900

    [CalculatorProject] Level2 구현
    1. Calculator 클래스 구현
    2. CalculatorApp 클래스에서 메인 메뉴 구현
    3. Getter / Setter 사용하여 계산 결과 리스트 저장 / 조회할 수 있게 구현
    4. 결과 리스트 중 가장 먼저 저장된 값 삭제되는 기능 구현
    5. 피연산자/연산자 예외처리 구현

--- src/com/example/calculator/Level2/Calculator.java
+++ src/com/example/calculator/Level2/Calculator.java
@@ -1,4 +1,41 @@
 package com.example.calculator.Level2;
 
+import java.util.ArrayList;
+
 public class Calculator {
+    ArrayList<Integer> resultList = new ArrayList<>();
+
+    public ArrayList<Integer> getResultList(){
+        return this.resultList;
+    }
+
+    public void setResultList(int result){
+        this.resultList.add(result);
+    }
+    public int calOperation(int firstNumber, int secondNumber, String operator){
+        int result = 0;
+        switch(operator){
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                if(secondNumber == 0){
+                    throw new ArithmeticException("나누기 값이 0입니다.");
+                }
+                result = firstNumber / secondNumber;
+                break;
+            default:
+        }
+        return result;
+    }
+
+    public void removeResult(){
+        this.resultList.remove(0);
+    }
 }
--- src/com/example/calculator/Level2/CalculatorApp.java
+++ src/com/example/calculator/Level2/CalculatorApp.java
@@ -1,4 +1,101 @@
 package com.example.calculator.Level2;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class CalculatorApp {
+    public static void main(String[] args) throws Exception {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean flag = false;
+        while(true){
+            if(flag) break;
+            System.out.println("원하시는 동작을 숫자로 입력하세요.");
+            System.out.println("1. 계산하기 | 2. 이전 계산 결과 확인 | 3. 삭제하기 | 4. 종료하기");
+            String command = scanner.nextLine();
+            switch(command){
+                case "1":
+                    System.out.println("**계산 시작**");
+                    calculator(calculator);
+                    break;
+                case "2":
+                    System.out.println("결과 확인 : " + calculator.getResultList());
+                    break;
+                case "3":
+                    calculator.removeResult();
+                    System.out.println("첫번째 계산 결과가 삭제되었습니다.");
+                    System.out.println("결과 확인 : " + calculator.getResultList());
+                    break;
+                case "4":
+                    flag = true;
+                    break;
+                default:
+                    System.out.println("원하시는 동작이 존재하지 않습니다. 다시 입력해주세요.");
+                    System.out.println("------------------------------------------------------");
+            }
+        }
+    }
+
+    public static void calculator(Calculator calculator) throws Exception{
+        int result = 0;
+        boolean parseFlag = false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("첫번째 숫자를 입력하세요");
+        String firstString = scanner.nextLine();
+        System.out.println("두번째 숫자를 입력하세요");
+        String secondString = scanner.nextLine();
+
+        try{
+            parseOperand(firstString);
+            parseOperand(secondString);
+        }catch (NumberFormatException e){
+            System.out.println("올바른 숫자 형식이 아닙니다. " + e.getMessage()+ "로 입력하세요.");
+            System.out.println("------------------------------------------------------");
+            return;
+        }
+        int firstNumber = Integer.parseInt(firstString);
+        int secondNumber = Integer.parseInt(secondString);
+
+        System.out.println("원하시는 사칙 연산을 입력하세요");
+        String operator = scanner.nextLine();
+        try{
+            parseOperator(operator);
+        }catch (NumberFormatException e){
+            System.out.println("올바른 연산자 형식이 아닙니다. " + e.getMessage()+ "로 입력하세요.");
+            System.out.println("------------------------------------------------------");
+            return;
+        }
+
+        try{
+            result = calculator.calOperation(firstNumber, secondNumber, operator);
+            calculator.setResultList(result);
+        } catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        System.out.println("계산 결과 : " + result);
+    }
+
+    public static void parseOperand(String strParse) throws Exception {
+        final String NUMBER_REQ = "^[0-9]*$";
+
+        boolean regex = Pattern.matches(NUMBER_REQ, strParse);
+        if(!regex){
+            throw new NumberFormatException("정수");
+        }
+    }
+
+    public static void parseOperator(String strParse) throws Exception {
+        final String OPERATION_REQ = "[+\\-*/]";
+
+        boolean regex = Pattern.matches(OPERATION_REQ, strParse);
+        if(!regex){
+            throw new NumberFormatException("+, -, *, /");
+        }
+    }
 }
+
+
