fbaa58ccc95a0f0a9bbf1aac614d90a3100e6b92
sumyeom <wjstndus75@naver.com>
2024-10-17 10:16:20 +0900

    [CalculatorProject] Level1 구현
    1. 메서드 명 이름 수정
    2. 주석 추가

--- src/com/example/calculator/Level1/Calculator.java
+++ src/com/example/calculator/Level1/Calculator.java
@@ -9,19 +9,23 @@ public class Calculator {
         double answer = 0;
 
         while(true) {
+            // 첫번째 숫자 입력
             System.out.println("첫번째 숫자를 입력하세요");
             String firstString = scanner.nextLine();
             double firstNumber = Double.parseDouble(firstString);
 
+            // 두번째 숫자 입력
             System.out.println("두번째 숫자를 입력하세요");
             String secondString = scanner.nextLine();
             double secondNumber = Double.parseDouble(secondString);
 
+            // 연산자 입력
             System.out.println("원하시는 사칙 연산을 입력하세요");
             String operator = scanner.nextLine();
             char charOperator = operator.charAt(0);
 
-            answer = operation(firstNumber, secondNumber,charOperator);
+            // 계산 수행
+            answer = Operation(firstNumber, secondNumber,charOperator);
             System.out.println("결과 : " + answer);
 
             System.out.println("계산을 더 할까요?(아무거나 클릭 시 계속 수행 / exit 입력 시 종료)");
@@ -32,7 +36,7 @@ public class Calculator {
         }
     }
 
-    public static double operation(double firstNumber, double secondNumber, char operator){
+    public static double Operation(double firstNumber, double secondNumber, char operator){
         double operationAnswer = 0;
         switch (operator) {
             case '+' :
